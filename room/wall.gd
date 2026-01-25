8d0341cd88e8aca7e9d4b3dcd3181aab24f7b843
denniscarr <dcarr1@gmail.com>
2026-01-24 20:45:01 -0500

    Hook up doors in wall scripting

--- room/wall.gd
+++ room/wall.gd
@@ -3,8 +3,10 @@ extends Node
 
 @export var _door_tile_1: MeshInstance3D
 @export var _door_tile_2: MeshInstance3D
+@export var _door: Node
 
 
 func create_door():
-    _door_tile_1.visible = false
-    _door_tile_2.visible = false
+	_door_tile_1.visible = false
+	_door_tile_2.visible = false
+	_door.visible = true
